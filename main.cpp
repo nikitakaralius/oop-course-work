75ea154512b7aeac833720b109f451e05b2d436a
Каралюс Никита <nkaralius@data-platform.ru>
2023-12-16 12:59:11 +0300

    Scaffolded main()

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
 #include "Infrastructure/ApplicationBuilder/ApplicationBuilder.h"
+#include "Infrastructure/MIddleware/ConsoleLoggerMiddleware/ConsoleLoggerMiddleware.h"
+#include "Infrastructure/Services/ApplicationRequestRouter/ApplicationRequestRouter.h"
 
 
 int main() {
-    ApplicationBuilder::create()
-    .withRequestRouter()
+    const auto app = ApplicationBuilder::create()
+            .withRequestRouter(new ApplicationRequestRouter)
+            .build();
 
+    app->use(new ConsoleLoggerMiddleware);
+    app->run();
+
+    delete app;
     return 0;
 }
