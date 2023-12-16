ffe95681ec4a7fcea6b74a39890ce221282992c1
Каралюс Никита <nkaralius@data-platform.ru>
2023-12-16 21:55:56 +0300

    Fixed compilation issue

--- Features/Common/FilesRequestHandler.h
+++ Features/Common/FilesRequestHandler.h
@@ -21,5 +21,46 @@ private:
         int maxDepthLevel) const;
 };
 
+template<class TRequest, class TResponse>
+std::vector<FileEntry*> FilesRequestHandler<TRequest, TResponse>::getFiles(const FilesRequest& request) const {
+    auto rootDirectory = DirectoryEntry(request.getDirectoryPath());
+    return getFiles(rootDirectory, 1, request.getMaxDepthLevel());
+}
+
+template<class TRequest, class TResponse>
+std::vector<FileEntry*> FilesRequestHandler<TRequest, TResponse>::getFiles(
+    const DirectoryEntry& directory,
+    int currentDepthLevel,
+    int maxDepthLevel) const {
+    if (currentDepthLevel > maxDepthLevel)
+        return {};
+
+    std::vector<FileEntry*> files;
+    auto directoryFiles = directory.files();
+
+    files.insert(
+        files.end(),
+        directoryFiles.begin(),
+        directoryFiles.end());
+
+    auto subdirectories = directory.subdirectories();
+
+    if (subdirectories.empty())
+        return files;
+
+    for (const auto subdirectory : subdirectories) {
+        auto subdirectoryFiles = getFiles(
+            *subdirectory,
+            currentDepthLevel + 1,
+            maxDepthLevel);
+
+        files.insert(
+            files.end(),
+            subdirectoryFiles.begin(),
+            subdirectoryFiles.end());
+    }
+
+    return files;
+}
 
 #endif //FILESREQUESTHANDLER_H
