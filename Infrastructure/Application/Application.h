1286bcb35a2f01a85d359b8b21f35a5faf4863e4
Каралюс Никита <nkaralius@data-platform.ru>
2023-12-16 12:43:58 +0300

    Created Application and ApplicationBuilder

---
+++ Infrastructure/Application/Application.h
@@ -0,0 +1,16 @@
+//
+// Created by Nikita Karalyus on 16/12/2023.
+//
+
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+
+
+class Application {
+
+};
+
+
+
+#endif //APPLICATION_H
---
+++ Infrastructure/ApplicationBuilder/ApplicationBuilder.h
@@ -0,0 +1,16 @@
+//
+// Created by Nikita Karalyus on 16/12/2023.
+//
+
+#ifndef APPLICATIONBUILDER_H
+#define APPLICATIONBUILDER_H
+
+
+
+class ApplicationBuilder {
+
+};
+
+
+
+#endif //APPLICATIONBUILDER_H
